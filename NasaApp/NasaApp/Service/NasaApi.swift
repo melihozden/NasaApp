6d9953acf9c7ed4c3a3d07255ecdef40f6054b3d
Melih <melihozden403@gmail.com>
2022-08-20 16:48:58 +0300

    API key structed

---
+++ NasaApp/NasaApp/Service/NasaApi.swift
@@ -0,0 +1,15 @@
+//
+//  NasaApi.swift
+//  NasaApp
+//
+//  Created by Melih Ozden on 20.08.2022.
+//
+
+import Foundation
+
+struct NasaApi{
+    let baseUrlRovers: String = "https://api.nasa.gov"
+    // TODO: It will be hidden in .plist & .gitignore
+    let demoKey: String = "Q4GdvPFnMzJSdi9hqHmn4w8tr2wWHo7x9NuVuxVW" // <DEMO_KEY>
+    let sol: Int = 1000
+}
--- NasaApp/NasaApp/Service/NasaService.swift
+++ NasaApp/NasaApp/Service/NasaService.swift
@@ -9,10 +9,11 @@ import Foundation
 import Alamofire
 
 class NasaService {
+    let api = NasaApi()
     
     func getPhotos(roverType: RoverType, page: Int, completion: @escaping ([Photo]) -> ()) {
      
-        guard let url = URL(string: "https://api.nasa.gov/mars-photos/api/v1/rovers/\(roverType)/photos?sol=1000&api_key=Q4GdvPFnMzJSdi9hqHmn4w8tr2wWHo7x9NuVuxVW&page=\(page)") else { return }
+        guard let url = URL(string: "\(api.baseUrlRovers)/mars-photos/api/v1/rovers/\(roverType)/photos?sol=\(api.sol)&api_key=\(api.demoKey)&page=\(page)") else { return }
         
         AF.request(url, method: .get).responseDecodable(of: PhotoResponse.self) { response in
             switch response.result {
